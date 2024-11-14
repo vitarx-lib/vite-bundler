ac8624d826f67ab71e8ce17a52b47a58e9870527
ZhuChongLin <8210856@qq.com>
2024-11-14 15:47:29 +0800

    feat: 添加 Vite 插件实现 JSX 和 TSX 文件处理
    - 新增 Vite 插件 "vite-plugin-vitarx"- 实现对 .jsx 和 .tsx 文件的处理
    - 配置 Esbuild 以支持 JSX
    - 添加源码映射配置

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,37 @@
-console.log('Happy developing ✨')
+import { Plugin, type ResolvedConfig } from 'vite'
+import handleJsxOrTsxFileCode from './jsx-handler.js'
+
+export default function vitePluginVitarx(): Plugin {
+  let sourcemap: boolean | 'inline' | 'hidden' = false
+  return {
+    name: 'vite-plugin-vitarx',
+    config(config) {
+      if (!config.esbuild) {
+        config.esbuild = {
+          jsx: 'automatic',
+          jsxImportSource: 'vitarx/dist'
+        }
+      } else {
+        config.esbuild.jsx = 'automatic'
+        config.esbuild.jsxImportSource = 'vitarx/dist'
+      }
+      return config
+    },
+    configResolved(config: ResolvedConfig) {
+      sourcemap = config.build.sourcemap
+    },
+    transform(code, id) {
+      // 仅处理 .jsx 或 .tsx 文件
+      if (id.endsWith('.jsx') || id.endsWith('.tsx')) {
+        return handleJsxOrTsxFileCode(code, {
+          sourceMaps: sourcemap !== 'hidden',
+          sourceFileName: id
+        })
+      }
+      return {
+        code,
+        map: null
+      }
+    }
+  }
+}
