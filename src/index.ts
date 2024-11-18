55258dd2a81479c4d1877b582fdc961893478b94
ZhuChongLin <8210856@qq.com>
2024-11-19 03:20:09 +0800

    refactor: 移除未使用的 HMR 相关代码
    - 删除了不必要的 isHMRUpdate函数
    - 清理了与 HMR 无关的导入和导出

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,6 @@
 import { Plugin, type ResolvedConfig } from 'vite'
 import handleJsxOrTsxFileCode from './jsx-handler.js'
 
-/**
- * 判断是否需要热更新
- *
- * @param file
- */
-function isHMRUpdate(file: string) {
-  const regex = /\.(jsx?|tsx?|css|scss|sass|less|styl|pcss|postcss)$/
-  return regex.test(file)
-}
-
 export default function vitePluginVitarx(): Plugin {
   let vite_config: ResolvedConfig = {} as ResolvedConfig
   return {
