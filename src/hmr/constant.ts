90f209053c98d5e5c99215f5843f65706546fed3
ZhuChongLin <8210856@qq.com>
2024-11-30 12:00:02 +0800

    refactor(hmr): 添加 HMR 相关的常量标识符
    - 新增 HmrId枚举，定义了 HMR过程中使用的各种标识符- 包括 hmr、vnode、state、manager 和 cache等重要标识

---
+++ src/hmr/constant.ts
@@ -0,0 +1,10 @@
+/**
+ * 标识符
+ */
+export enum HmrId {
+  hmr = '__$vitarx_vite_hmr$__',
+  vnode = '__$vitarx_vnode$__',
+  state = '__$vitarx_state$__',
+  manager = '__$vitarx_hmr_manager$__',
+  cache = '__$vitarx_widget_hmr_map$__'
+}
