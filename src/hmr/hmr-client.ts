7184084a01108c2fcaa6c894ca48f77c66f860ed
ZhuChongLin <8210856@qq.com>
2024-12-19 23:50:45 +0800

    refactor(hmr): 优化 widget 更新逻辑
    - 将 updateWidget 函数拆分为 updateWidgetBuild 和 updateWidgetFull 两个函数
    - 使用 createWidgetVNodeInstance 替代直接创建新实例的方式
    - 优化了属性恢复和子节点更新的逻辑

--- src/hmr/hmr-client.ts
+++ src/hmr/hmr-client.ts
@@ -77,8 +77,9 @@ function handleHmrUpdate(vnode: VNODE, newModule: WidgetType) {
     if (change.other) delete vnode.__$vitarx_state$__
   }
   if (!change.build && !change.other) return
-  const newInstance = createWidgetVNodeInstance(vnode)
-  updateWidget(newInstance, oldInstance, oldScope, change)
+  createWidgetVNodeInstance(vnode, newInstance => {
+    updateWidget(newInstance, oldInstance, oldScope, change)
+  })
 }
 
 /**
@@ -96,61 +97,71 @@ function updateWidget(
   change: ChangeCode
 ): void {
   if (change.build && !change.other) {
-    // 销毁旧作用域
-    oldScope.destroy()
-    // 恢复属性
-    for (const key in oldInstance) {
-      if (__widgetIntrinsicPropKeywords__.includes(key as any)) continue
-      const oldValue = (oldInstance as any)[key]
-      // 仅恢复非副作用属性
-      if (!isEffect(oldValue)) (newInstance as any)[key] = oldValue
-    }
-    // 新子节点
-    const newChild = newInstance.renderer.child
-    // 恢复旧的子节点
-    newInstance.renderer['_child'] = oldInstance.renderer.child
-    // 恢复渲染器状态
-    newInstance.renderer['_state'] = oldInstance.renderer.state
-    // 恢复占位元素
-    newInstance.renderer['_shadowElement'] = oldInstance.renderer['_shadowElement']
-    // 恢复传送节点父元素
-    newInstance.renderer['_teleport'] = oldInstance.renderer['_teleport']
-    // 重置小部件实例
-    newInstance.vnode.instance = newInstance
-    // 更新引用
-    newInstance.vnode.ref && (newInstance.vnode.ref.value = newInstance)
-    // 更新新的子节点到旧子节点
-    newInstance.renderer.update(newChild)
+    updateWidgetBuild(newInstance, oldInstance, oldScope)
   } else {
-    // 如果是非活跃状态则不更新js逻辑层与数据层的变化
-    if (oldInstance.renderer.state !== 'activated') return
-    // 创建占位元素
-    const placeholderEl: Text = document.createTextNode('')
-    // 父元素
-    let parentEl: ParentNode
-    // 如果旧实例使用了传送功能
-    if (oldInstance.renderer.teleport) {
-      // 占位节点插入到影子元素之前
-      parentEl = insertBeforeExactly(placeholderEl, oldInstance.renderer.shadowElement)
-    } else {
-      // 占位节点插入到旧元素之前
-      parentEl = insertBeforeExactly(placeholderEl, oldInstance.renderer.el!)
-    }
-    // 卸载旧的组件实例
-    oldInstance.renderer.unmount(false)
-    // 渲染新元素
-    const el = newInstance.renderer.render()
-    // 如果是传送节点
-    if (newInstance.renderer.teleport) {
-      // 用影子元素替换掉占位元素，新组件实例挂载时自动将真实的元素挂载到传送节点中！
-      parentEl.replaceChild(newInstance.renderer.shadowElement, placeholderEl)
-    } else {
-      // 非占位节点用新元素替换占位元素
-      parentEl.replaceChild(el, placeholderEl)
-    }
-    // 挂载完成
-    newInstance.renderer.mount()
+    updateWidgetFull(newInstance, oldInstance)
   }
+}
+
+// 更新build部分
+function updateWidgetBuild(newInstance: Widget, oldInstance: Widget, oldScope: Scope): void {
+  // 销毁旧作用域
+  oldScope.destroy()
+  // 恢复属性
+  for (const key in oldInstance) {
+    if (__widgetIntrinsicPropKeywords__.includes(key as any)) continue
+    const oldValue = (oldInstance as any)[key]
+    // 仅恢复非副作用属性
+    if (!isEffect(oldValue)) (newInstance as any)[key] = oldValue
+  }
+  // 新子节点
+  const newChild = newInstance.renderer.child
+  // 恢复旧的子节点
+  newInstance.renderer['_child'] = oldInstance.renderer.child
+  // 恢复渲染器状态
+  newInstance.renderer['_state'] = oldInstance.renderer.state
+  // 恢复占位元素
+  newInstance.renderer['_shadowElement'] = oldInstance.renderer['_shadowElement']
+  // 恢复传送节点父元素
+  newInstance.renderer['_teleport'] = oldInstance.renderer['_teleport']
+  // 重置小部件实例
+  newInstance.vnode.instance = newInstance
+  // 更新引用
+  newInstance.vnode.ref && (newInstance.vnode.ref.value = newInstance)
+  // 更新新的子节点到旧子节点
+  newInstance.renderer.update(newChild)
+}
+
+// 全量更新
+function updateWidgetFull(newInstance: Widget, oldInstance: Widget): void {
+  // 如果是非活跃状态则不更新js逻辑层与数据层的变化
+  if (oldInstance.renderer.state !== 'activated') return
+  // 创建占位元素
+  const placeholderEl: Text = document.createTextNode('')
+  // 父元素
+  let parentEl: ParentNode
+  // 如果旧实例使用了传送功能
+  if (oldInstance.renderer.teleport) {
+    // 占位节点插入到影子元素之前
+    parentEl = insertBeforeExactly(placeholderEl, oldInstance.renderer.shadowElement)
+  } else {
+    // 占位节点插入到旧元素之前
+    parentEl = insertBeforeExactly(placeholderEl, oldInstance.renderer.el!)
+  }
+  // 卸载旧的组件实例
+  oldInstance.renderer.unmount(false)
+  // 渲染新元素
+  const el = newInstance.renderer.render()
+  // 如果是传送节点
+  if (newInstance.renderer.teleport) {
+    // 用影子元素替换掉占位元素，新组件实例挂载时自动将真实的元素挂载到传送节点中！
+    parentEl.replaceChild(newInstance.renderer.shadowElement, placeholderEl)
+  } else {
+    // 非占位节点用新元素替换占位元素
+    parentEl.replaceChild(el, placeholderEl)
+  }
+  // 挂载完成
+  newInstance.renderer.mount()
 }
 
 /**
