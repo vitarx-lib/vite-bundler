c587d74795949ed0ae50184e7f056a3c98e950f3
ZhuChongLin <8210856@qq.com>
2024-12-20 22:15:22 +0800

    refactor(hmr): 优化组件热更新逻辑
    - 重构了 getState 函数，使用 HmrId.state 代替硬编码的属性名
    - 优化了 updateWidget函数，移除了不必要的参数和逻辑
    - 重构了 updateWidgetBuild 和 updateWidgetFull 函数，提高了代码复用性
    - 使用更简洁的属性访问方式，如 oldInstance['renderer']，提高了代码可读性
    - 删除了不必要的类型断言，简化了类型检查

--- src/hmr/hmr-client.ts
+++ src/hmr/hmr-client.ts
@@ -2,6 +2,7 @@
 
 import {
   createWidgetVNodeInstance,
+  FnWidget,
   isClassWidgetConstructor,
   isEffect,
   type Scope,
@@ -22,7 +23,7 @@ declare global {
 }
 
 type VNODE<T extends WidgetType = WidgetType> = VNode<T> & {
-  __$vitarx_state$__: Record<string, any>
+  __$hmr_state$__?: Record<string, any>
   instance: Widget
   scope: Scope
 }
@@ -37,7 +38,7 @@ type VNODE<T extends WidgetType = WidgetType> = VNode<T> & {
  * @param name
  */
 export function getState(vnode: VNODE, name: string) {
-  const state = vnode?.__$vitarx_state$__?.[name]
+  const state = vnode?.[HmrId.state]?.[name]
   // 如果是副作用，则丢弃。
   if (state && isEffect(state)) return undefined
   return state
@@ -65,20 +66,19 @@ function handleHmrUpdate(vnode: VNODE, newModule: WidgetType) {
   const oldModule = vnode.type
   // 更新虚拟节点中的组件构造函数
   vnode.type = newModule
-  if (['unloaded', 'uninstalling'].includes(vnode.instance.renderer.state)) return
+  if (['unloaded', 'uninstalling'].includes(vnode.instance['renderer'].state)) return
   const oldInstance = vnode.instance
-  const oldScope = vnode.scope
   let change: ChangeCode
-  if (isClassWidgetConstructor(vnode.type)) {
+  const isClass = isClassWidgetConstructor(vnode.type)
+  if (isClass) {
     change = differenceClassWidgetChange(newModule.toString(), oldModule.toString())
+    if (!change.other) vnode[HmrId.state] = {}
   } else {
     change = differenceFnWidgetChange(newModule.toString(), oldModule.toString())
-    // @ts-ignore 如果非只更新渲染视图，则删除保留的状态
-    if (change.other) delete vnode.__$vitarx_state$__
   }
-  if (!change.build && !change.other) return
+  if (change.other) delete vnode[HmrId.state]
   createWidgetVNodeInstance(vnode, newInstance => {
-    updateWidget(newInstance, oldInstance, oldScope, change)
+    updateWidget(newInstance, oldInstance, change, isClass)
   })
 }
 
@@ -87,81 +87,79 @@ function handleHmrUpdate(vnode: VNODE, newModule: WidgetType) {
  *
  * @param newInstance
  * @param oldInstance
- * @param oldScope
  * @param change
+ * @param isClass
  */
 function updateWidget(
   newInstance: Widget,
   oldInstance: Widget,
-  oldScope: Scope,
-  change: ChangeCode
+  change: ChangeCode,
+  isClass: boolean
 ): void {
   if (change.build && !change.other) {
-    updateWidgetBuild(newInstance, oldInstance, oldScope)
+    updateWidgetBuild(newInstance, oldInstance, isClass)
   } else {
     updateWidgetFull(newInstance, oldInstance)
   }
 }
 
 // 更新build部分
-function updateWidgetBuild(newInstance: Widget, oldInstance: Widget, oldScope: Scope): void {
+function updateWidgetBuild(newInstance: Widget, oldInstance: Widget, isClass: boolean): void {
   // 销毁旧作用域
-  oldScope.destroy()
-  // 恢复属性
-  for (const key in oldInstance) {
-    if (__widgetIntrinsicPropKeywords__.includes(key as any)) continue
-    const oldValue = (oldInstance as any)[key]
-    // 仅恢复非副作用属性
-    if (!isEffect(oldValue)) (newInstance as any)[key] = oldValue
+  oldInstance['renderer'].scope.destroy()
+  // 类组件恢复属性
+  if (isClass) {
+    // 恢复属性
+    for (const key in oldInstance) {
+      if (__widgetIntrinsicPropKeywords__.includes(key as any)) continue
+      const oldValue = (oldInstance as any)[key]
+      // 仅恢复非副作用属性
+      if (!isEffect(oldValue)) (newInstance as any)[key] = oldValue
+    }
   }
-  // 新子节点
-  const newChild = newInstance.renderer.child
   // 恢复旧的子节点
-  newInstance.renderer['_child'] = oldInstance.renderer.child
+  newInstance['renderer']['_child'] = oldInstance['renderer'].child
   // 恢复渲染器状态
-  newInstance.renderer['_state'] = oldInstance.renderer.state
+  newInstance['renderer']['_state'] = oldInstance['renderer'].state
   // 恢复占位元素
-  newInstance.renderer['_shadowElement'] = oldInstance.renderer['_shadowElement']
+  newInstance['renderer']['_shadowElement'] = oldInstance['renderer']['_shadowElement']
   // 恢复传送节点父元素
-  newInstance.renderer['_teleport'] = oldInstance.renderer['_teleport']
-  // 重置小部件实例
-  newInstance.vnode.instance = newInstance
+  newInstance['renderer']['_teleport'] = oldInstance['renderer']['_teleport']
   // 更新引用
-  newInstance.vnode.ref && (newInstance.vnode.ref.value = newInstance)
-  // 更新新的子节点到旧子节点
-  newInstance.renderer.update(newChild)
+  newInstance['vnode'].ref && (newInstance['vnode'].ref.value = newInstance)
+  if (!isClass) newInstance['renderer'].update()
 }
 
 // 全量更新
 function updateWidgetFull(newInstance: Widget, oldInstance: Widget): void {
   // 如果是非活跃状态则不更新js逻辑层与数据层的变化
-  if (oldInstance.renderer.state !== 'activated') return
+  if (oldInstance['renderer'].state !== 'activated') return
   // 创建占位元素
   const placeholderEl: Text = document.createTextNode('')
   // 父元素
   let parentEl: ParentNode
   // 如果旧实例使用了传送功能
-  if (oldInstance.renderer.teleport) {
+  if (oldInstance['renderer'].teleport) {
     // 占位节点插入到影子元素之前
-    parentEl = insertBeforeExactly(placeholderEl, oldInstance.renderer.shadowElement)
+    parentEl = insertBeforeExactly(placeholderEl, oldInstance['renderer'].shadowElement)
   } else {
     // 占位节点插入到旧元素之前
-    parentEl = insertBeforeExactly(placeholderEl, oldInstance.renderer.el!)
+    parentEl = insertBeforeExactly(placeholderEl, oldInstance['renderer'].el!)
   }
   // 卸载旧的组件实例
-  oldInstance.renderer.unmount(false)
+  oldInstance['renderer'].unmount(true)
   // 渲染新元素
-  const el = newInstance.renderer.render()
+  const el = newInstance['renderer'].render()
   // 如果是传送节点
-  if (newInstance.renderer.teleport) {
+  if (newInstance['renderer'].teleport) {
     // 用影子元素替换掉占位元素，新组件实例挂载时自动将真实的元素挂载到传送节点中！
-    parentEl.replaceChild(newInstance.renderer.shadowElement, placeholderEl)
+    parentEl.replaceChild(newInstance['renderer'].shadowElement, placeholderEl)
   } else {
     // 非占位节点用新元素替换占位元素
     parentEl.replaceChild(el, placeholderEl)
   }
   // 挂载完成
-  newInstance.renderer.mount()
+  newInstance['renderer'].mount()
 }
 
 /**
