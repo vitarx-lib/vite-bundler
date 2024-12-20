f5f7ce441a1e25adbcc16daf0c338281dffbbcd0
ZhuChongLin <8210856@qq.com>
2024-12-20 22:17:52 +0800

    refactor(hmr): 修改 state属性名称以增强代码可读性
    - 将 HmrId枚举中的 state 属性从 '__$vitarx_state$__' 修改为 '__$hmr_state$__'- 此修改旨在提高代码的可读性和一致性，同时保留原有功能
    
    Signed-off-by: ZhuChongLin <8210856@qq.com>

--- src/hmr/constant.ts
+++ src/hmr/constant.ts
@@ -4,7 +4,7 @@
 export enum HmrId {
   hmr = '__$vitarx_vite_hmr$__',
   vnode = '__$vitarx_vnode$__',
-  state = '__$vitarx_state$__',
+  state = '__$hmr_state$__',
   manager = '__$vitarx_hmr_manager$__',
   cache = '__$vitarx_widget_hmr_map$__'
 }
