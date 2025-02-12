e7822f091fdafb6eba6e95d43a14177827c4ddb3
ZhuChongLin <8210856@qq.com>
2025-02-12 22:04:39 +0800

    feat: 添加 HMR 客户端的路径解析
    - 使用 defineConfig 和 mergeConfig 来定义Vite 配置
    - 添加 resolve 配置，设置 @vitarx/vite-bundler/client 的别名路径
    - 引入 path 模块以获取当前文件目录路径
    
    Signed-off-by: ZhuChongLin <8210856@qq.com>

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
-import { mergeConfig, Plugin, type ResolvedConfig } from 'vite'
+import { defineConfig, mergeConfig, Plugin, type ResolvedConfig } from 'vite'
 import handleJsxOrTsxFileCode from './jsx-handler.js'
+import * as path from 'node:path'
 
+const __dirname = path.dirname(new URL(import.meta.url).pathname)
 /**
  * ## vite-plugin-vitarx
  *
@@ -11,12 +13,20 @@ export default function vitarx(): Plugin {
   return {
     name: 'vite-plugin-vitarx',
     config(config) {
-      return mergeConfig(config, {
-        esbuild: {
-          jsx: 'automatic',
-          jsxImportSource: 'vitarx'
-        }
-      })
+      return mergeConfig(
+        config,
+        defineConfig({
+          esbuild: {
+            jsx: 'automatic',
+            jsxImportSource: 'vitarx'
+          },
+          resolve: {
+            alias: {
+              '@vitarx/vite-bundler/client': path.resolve(__dirname, 'hmr/hmr-client.js')
+            }
+          }
+        })
+      )
     },
     configResolved(config: ResolvedConfig) {
       vite_config = config
