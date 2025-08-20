f9e48fcc6312e80f402d9286bc1ec5a699a1ee18
ZhuChongLin <8210856@qq.com>
2025-08-21 01:35:09 +0800

    refactor(hmr-client): 重构组件热更新逻辑
    - 优化了组件实例的更新流程，提高了热更新的效率和稳定性
    - 重构了 updateWidgetBuild 和 updateWidgetFull 函数，简化了组件更新逻辑
    - 使用 WidgetVNode 替代 VNode<WidgetType>，提高了类型定义的准确性- 用 $vnode 替代 renderer 访问组件的虚拟节点，统一了代码风格
    - 优化了占位元素和父元素的处理逻辑，提高了组件更新的灵活性
    
    Signed-off-by: ZhuChongLin <8210856@qq.com>

--- src/hmr-client/update.ts
+++ src/hmr-client/update.ts
@@ -1,13 +1,11 @@
 import { type ChangeCode, differenceClassWidgetChange, differenceFnWidgetChange } from './utils.js'
 import {
-  __widgetIntrinsicPropKeywords__,
-  createWidgetVNodeInstance,
-  isClassWidgetConstructor,
+  __WIDGET_INTRINSIC_KEYWORDS__,
+  DomHelper,
   isEffect,
-  type VNode,
-  WebRuntimeDom,
-  type Widget,
-  type WidgetType
+  Widget,
+  type WidgetType,
+  type WidgetVNode
 } from 'vitarx'
 import { HmrId } from './constant.js'
 
@@ -35,89 +33,74 @@ function updateWidget(
 // 更新build部分
 function updateWidgetBuild(newInstance: Widget, oldInstance: Widget, isClass: boolean): void {
   // 销毁旧作用域
-  oldInstance['renderer'].scope.destroy()
+  oldInstance.$scope.dispose()
   // 类组件恢复属性
   if (isClass) {
     // 恢复属性
     for (const key in oldInstance) {
-      if (__widgetIntrinsicPropKeywords__.includes(key as any)) continue
+      if (__WIDGET_INTRINSIC_KEYWORDS__.includes(key as any)) continue
       const oldValue = (oldInstance as any)[key]
       // 仅恢复非副作用属性
       if (!isEffect(oldValue)) (newInstance as any)[key] = oldValue
     }
   }
   // 新子节点
-  const newChild = newInstance['renderer'].child
-  // 恢复旧的子节点
-  newInstance['renderer']['_child'] = oldInstance['renderer'].child
-  // 恢复渲染器状态
-  newInstance['renderer']['_state'] = oldInstance['renderer'].state
-  // 恢复占位元素
-  newInstance['renderer']['_shadowElement'] = oldInstance['renderer']['_shadowElement']
-  // 恢复传送节点父元素
-  newInstance['renderer']['_teleport'] = oldInstance['renderer']['_teleport']
-  // 更新引用
-  newInstance['vnode'].ref && (newInstance['vnode'].ref.value = newInstance)
+  const newChild = newInstance.$vnode.child
+  newInstance.$vnode.recover(oldInstance.$vnode)
   // 更新视图，仅在组件是活跃状态下更新！！！
-  if (newInstance['renderer'].state === 'activated') newInstance['renderer'].update(newChild)
+  if (newInstance.$vnode.state === 'activated') newInstance.$vnode.updateChild(newChild)
 }
 
 // 全量更新
 function updateWidgetFull(newInstance: Widget, oldInstance: Widget): void {
   // 如果是非活跃状态则不更新js逻辑层与数据层的变化
-  if (oldInstance['renderer'].state !== 'activated') return
+  if (oldInstance.$vnode.state !== 'activated') return
   // 创建占位元素
   const placeholderEl: Text = document.createTextNode('')
   // 父元素
   let parentEl: ParentNode
   // 如果旧实例使用了传送功能
-  if (oldInstance['renderer'].teleport) {
+  if (oldInstance.$vnode.teleport) {
     // 占位节点插入到影子元素之前
-    parentEl = WebRuntimeDom.insertBeforeExactly(
-      placeholderEl,
-      oldInstance['renderer'].shadowElement
-    )
+    parentEl = DomHelper.insertBefore(placeholderEl, oldInstance.$vnode.shadowElement)
   } else {
     // 占位节点插入到旧元素之前
-    parentEl = WebRuntimeDom.insertBeforeExactly(placeholderEl, oldInstance['renderer'].el!)
+    parentEl = DomHelper.insertBefore(placeholderEl, oldInstance.$el)
   }
   // 卸载旧的组件实例
-  oldInstance['renderer'].unmount(true)
+  oldInstance.$vnode.unmount()
   // 渲染新元素
-  const el = newInstance['renderer'].render()
+  const el = newInstance.$vnode.render()
   // 如果是传送节点
-  if (newInstance['renderer'].teleport) {
+  if (newInstance.$vnode.teleport) {
     // 用影子元素替换掉占位元素，新组件实例挂载时自动将真实的元素挂载到传送节点中！
-    parentEl.replaceChild(newInstance['renderer'].shadowElement, placeholderEl)
+    parentEl.replaceChild(newInstance.$vnode.shadowElement, placeholderEl)
   } else {
     // 非占位节点用新元素替换占位元素
     parentEl.replaceChild(el, placeholderEl)
   }
   // 挂载完成
-  newInstance['renderer'].mount()
+  newInstance.$vnode.mount()
 }
 
 /**
  * 处理热更新
  *
- * @param vnode
- * @param newModule
+ * @param vnode - 组件节点
+ * @param newModule - 新的组件模块
  */
-export default function handleHmrUpdate(vnode: VNode<WidgetType>, newModule: WidgetType) {
+export default function handleHmrUpdate(vnode: WidgetVNode, newModule: WidgetType) {
   try {
     const oldModule = vnode.type
     // 更新虚拟节点中的组件构造函数
-    vnode.type = newModule
+    vnode['type'] = newModule
     // 如果节点没有渲染，或节点已销毁，则跳过
-    if (
-      !vnode.instance ||
-      ['unloaded', 'uninstalling'].includes(vnode.instance['renderer'].state)
-    ) {
+    if (['notRendered', 'unloaded', 'uninstalling'].includes(vnode.state)) {
       return
     }
     const oldInstance = vnode.instance
     let change: ChangeCode
-    const isClass = isClassWidgetConstructor(vnode.type)
+    const isClass = Widget.isClassWidget(vnode.type)
     if (isClass) {
       // 恢复静态属性
       for (const key of Object.keys(oldModule)) {
@@ -134,9 +117,7 @@ export default function handleHmrUpdate(vnode: VNode<WidgetType>, newModule: Wid
       change = differenceFnWidgetChange(newModule.toString(), oldModule.toString())
     }
     if (change.other) delete vnode[HmrId.state]
-    createWidgetVNodeInstance(vnode, newInstance => {
-      updateWidget(newInstance, oldInstance, change, isClass)
-    })
+    updateWidget(vnode.instance, oldInstance, change, isClass)
   } catch (e) {
     console.error('[VitarxViteBundler]：热更新模块时捕获到异常', e)
   }
