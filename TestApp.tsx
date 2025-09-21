c9cd004f32953fd460cf1267a7475fec2af2da3c
ZhuChongLin <8210856@qq.com>
2025-09-22 00:25:13 +0800

    test(TestApp): 添加TestClassWidget组件引入与使用
    - 在TestApp中引入并使用了新的TestClassWidget组件，用于展示测试文本内容。
    - 创建了TestClassWidget.tsx文件，定义了一个继承自Widget的TestClassApp类组件。
    - 重写了build方法返回span元素显示"TestApp+++"。
    
    Signed-off-by: ZhuChongLin <8210856@qq.com>

--- TestApp.tsx
+++ TestApp.tsx
@@ -1,7 +1,10 @@
+import TestClassApp from './TestClassWidget.js'
+
 export default function TestApp() {
   return (
     <div>
       <h1>测试</h1>
+      <TestClassApp></TestClassApp>
     </div>
   )
 }
